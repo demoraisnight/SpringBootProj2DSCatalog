62b60ec889aac9e0c6c6cd68211015d73989526f
vini <viniciusdemoraisrosa@gmail.com>
2023-06-02 19:31:40 -0300

    add more custom validation

---
+++ backend/dscatalog/src/test/java/com/example/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,40 @@
+package com.example.dscatalog.services.validation;
+
+import com.example.dscatalog.controllers.exceptions.FieldMessage;
+import com.example.dscatalog.dto.UserInsertDTO;
+import com.example.dscatalog.entities.User;
+import com.example.dscatalog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = repository.findByEmail(dto.getEmail());
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
